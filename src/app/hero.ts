e71c7fe4ed365f3bb88ae2fb8f5011aa16892076
pd95 <pd95@users.noreply.github.com>
2017-04-24 22:04:42 +0200

    Enhanced datastructure for more Hero attributes

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,12 @@
 export class Hero {
-    id: number;
-    name: string;
+    constructor(
+        public id: number,
+        public name: string,
+        public power?: string,
+        public alterEgo?: string
+    ) { }
+
+    static fromObject(object: any) {
+        return new this(object.id, object.name, object.power, object.alterEgo);
+    }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,18 +1,19 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      Hero.fromObject({ id: 11, name: 'Mr. Nice' }),
+      Hero.fromObject({ id: 12, name: 'Narco' }),
+      Hero.fromObject({ id: 13, name: 'Bombasto' }),
+      Hero.fromObject({ id: 14, name: 'Celeritas' }),
+      Hero.fromObject({ id: 15, name: 'Magneta' }),
+      Hero.fromObject({ id: 16, name: 'RubberMan' }),
+      Hero.fromObject({ id: 17, name: 'Dynama' }),
+      Hero.fromObject({ id: 18, name: 'Dr IQ' }),
+      Hero.fromObject({ id: 19, name: 'Magma' }),
+      Hero.fromObject({ id: 20, name: 'Tornado' })
     ];
     return { heroes };
   }
